7f75cb343525511e066809bb0c850b7de1b079ff
Adi Salimgereev <68709264+VerteXGMD@users.noreply.github.com>
2022-07-06 03:22:53 +0600

    Fix small orthographic mistake
    Change word "split" to "spit"

--- src/main/java/org/vertex/funnyspits/FunnySpits.java
+++ src/main/java/org/vertex/funnyspits/FunnySpits.java
@@ -15,7 +15,7 @@ public final class FunnySpits extends JavaPlugin {
 
         int spitIntensity = configuration.getInt("spit_intensity");
         if (spitIntensity < 1 || spitIntensity > 10) {
-            getLogger().severe("split_intensity value in config.yml must be in the interval of 1 to 10");
+            getLogger().severe("spit_intensity value in config.yml must be in the interval of 1 to 10");
         }
 
         new FunnySpitsCommands(this);
