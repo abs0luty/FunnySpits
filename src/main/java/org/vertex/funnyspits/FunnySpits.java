61ff01e81943f6b646f5b0e4268fb26678e5a1c1
NotSoChill00 <108801586+NotSoChill00@users.noreply.github.com>
2022-08-09 12:57:04 -0700

    no more intensity limit :cool:

--- src/main/java/org/vertex/funnyspits/FunnySpits.java
+++ src/main/java/org/vertex/funnyspits/FunnySpits.java
@@ -162,11 +162,6 @@ public final class FunnySpits extends JavaPlugin {
         ListenersManager listenersManager = new ListenersManager(this);
         listenersManager.registerListeners();
 
-        int spitIntensity = configuration.getInt("spit_intensity");
-        if (spitIntensity < 1 || spitIntensity > 10) {
-            getLogger().severe("spit_intensity value in config.yml must be in the interval of 1 to 10");
-        }
-
         new FunnySpitsCommands(this);
         new SpitCommand(this);
     }
